b93864f0a8979a5a0c6c25d5e391c85009c66611
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-01-04 09:30:55 -0800

    ф

---
+++ onload.js
@@ -0,0 +1,61 @@
+
+//Максимум не включается, минимум включается
+const Random = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+const Sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+const Execute = async(text, type) => {
+	let last_char;
+	for(const ch of text) {
+		document.getElementById("console").innerHTML += ch;
+		if(ch === last_char){
+			await Sleep(Random(25, 100));
+		}else if(
+			ch === '.' ||
+			ch === ',' ||
+			ch === '-' ||
+			ch === '+' ||
+			ch === '=' ||
+			ch === ' '
+		){
+			await Sleep(Random(140, 250));
+		}else{
+			await Sleep(Random(50, 250));
+		}
+		last_char = ch;
+	}
+}
+
+const Colors = {
+	"1": () => {
+			document.getElementById("console").style.setProperty('--color', "rgb(12 142 17)");
+		},
+	"2": () => {
+			document.getElementById("console").style.setProperty('--color', "#fff");
+		}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("console").innerHTML = " ";
+	document.getElementById("console").style.setProperty('--color', "#fff");
+	document.getElementById("launch").addEventListener("click", async () => {
+		const text = document.getElementById("data").value;
+		const type = document.getElementById("type").value;
+		const color_type = document.getElementById("color_type").value;
+		
+		Colors[color_type]();
+		
+		document.getElementById("console").innerHTML = " ";
+		
+		await Sleep(3000);
+		
+		Execute(text, type);
+	});
+});
